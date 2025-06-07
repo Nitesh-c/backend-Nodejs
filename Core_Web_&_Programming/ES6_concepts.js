eb98a85da889780c51a97de7ac7957d197252077
NITESH <niteshbhai0911@gmail.com>
2025-06-07 10:52:32 +0000

    ES6 concepts end and I practice it

---
+++ Core_Web_&_Programming/ES6_concepts.js
@@ -0,0 +1,42 @@
+// arrow function 
+let myFunc = (x)=> (x * x);
+
+// default params
+function userLoginMessage(userName = "sam"){
+    console.log(`Hello ${userName} Welcome`);
+}
+
+userLoginMessage("admin")
+
+// this is how we copy the object in deep
+let userProfile = {
+    "name": "nitesh",
+    "email": "niteshbhai123@gmail.com",
+    "password": "nitesh123"
+}
+
+let cloneUserProfile = Object.values(userProfile);
+cloneUserProfile.name = "karam";
+console.log(cloneUserProfile.name);
+console.log(userProfile.name);
+
+
+// spread operator 
+let name = "nitesh"
+let weiredName = [...name];
+console.log(weiredName);
+
+
+// destructuring
+
+let adminUser = {
+    "name":"nitesh",
+    "email":"nitesh@google.com"
+}
+
+let {"name":userName, email: userMail} = adminUser;  
+// here we can acces the value using the userName and userMail
+console.table([userName, userMail]);
+
+
+// module got the module_File1 and module_File2
---
+++ Core_Web_&_Programming/module_file1.js
@@ -0,0 +1,48 @@
+// modules help us to transfer variable and mainly functions from one file to other file
+// like in this file we have a big data like it is database
+
+
+export let users = [
+    {
+        name: "nitesh",
+        email:"nitesh.chatgpt.com"
+    },
+        {
+        name: "karam",
+        email:"karam.discord.com"
+    },
+        {
+        name: "dharam",
+        email:"dharam199@dharm.com"
+    },
+        {
+        name: "pritam",
+        email:"pritam@5445@tcs.com"
+    },
+        {
+        name: "ujjwal",
+        email:"ujjwal@gemini.ai.com"
+    },
+        {
+        name: "ram",
+        email:"ramkumar@gmail.com"
+    },
+        {
+        name: "lakshman",
+        email:"lakshman111@microsoft.com"
+    },
+        {
+        name: "ritik",
+        email:"ritik.12@gmail.com"
+    },
+        {
+        name: "roshan",
+        email:"roshan@gpt.com"
+    },
+        {
+        name: "netesha",
+        email:"nitesha@google.com"
+    }
+]
+
+// export default {users}
