a161c7157e05ef4a3c165796451a55420a09782d
NITESH <niteshbhai0911@gmail.com>
2025-06-09 16:53:59 +0000

    start learning promise and async and await

---
+++ Core_Web_&_Programming/Level-3(Asynchronous Pro.)/promises_obj.js
@@ -0,0 +1,8 @@
+// Promises is a object helps us to run the asyncrhnous task
+
+// let myFirstPro = new Promise((resolve, reject)=>{
+//     console.log("Hello World !");
+//     reject("Bye World")
+// })
+// myFirstPro.catch("erro")
+// console.log(myFirstPro)
