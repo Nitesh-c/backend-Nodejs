d518484915bcc3130a1fbf1e9067c94dda5dc448
Nitesh <niteshbhai0911@gmail.com>
2025-06-11 01:22:13 +0530

    day-2 start call-back hell

--- Core_Web_&_Programming/Level-3(Asynchronous Pro.)/xhr.js
+++ Core_Web_&_Programming/Level-3(Asynchronous Pro.)/xhr.js
@@ -1,30 +1,10 @@
-// const request = new XMLHttpRequest();
-// request.open("GET", "https://jsonplaceholder.typicode.com/posts/1");
+const xhr = new XMLHttpRequest();
+xhr.responseType = JSON;
+xhr.addEventListener("load",()=>{
+  console.log(xhr)
+});
 
-// request.onload = function() {
-  // const responseData = JSON.parse(request.responseText);
-  // console.log(responseData);
-  // console.log(JSON.parse(request.responseText));
-  // console.log(this.responseURL);
-  // console.log(this.responseText);
-  // console.log(this.responseXML);
-// }
-// request.send();
+xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
+xhr.send();
 
-
-
-// let make a callback hell
-// function  sendRequest(url, callback) {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open("GET", url);
-//  xhr.onload = function () {
-//     return (JSON.parse(xhr.responseText))
-//  }
-//   xhr.send();
-
-// }
-
-// let myData = sendRequest('https://dummyjson.com/users', () => {})
-// console.log(myData);
-
-
+console.log("Hello");
