864623215249255dddc4c08db87a7bbe284dc6a4
nitesh kumar <niteshbhai0911@gmail.com>
2025-06-21 07:46:48 +0530

    write some with the topic set() object in js and then end

--- Core_Web_&_Programming/set.js
+++ Core_Web_&_Programming/set.js
@@ -1,2 +1,21 @@
 // what is set object in javascript and why to use ?
 // ->> because it does't allow the duplicate value and if the duplicate value is give it ignore the duplicate value
+
+// lets create set();
+
+const user = new Set();
+user.add("nitesh")
+user.add("nitesh@gmail.com")
+user.add("nitesh")
+user.add("nitesh kumar");
+
+// console.table(user); // it's ignore the duplicate value
+
+
+// its methods
+// add(), delete(), clear(), has() ==> this all are methods, size ==> this is property
+
+let fruits = ["apple", "mango", "banana", "apple", "mango"];
+
+let freshFruits = new Set([...fruits]);
+console.log([freshFruits]);
