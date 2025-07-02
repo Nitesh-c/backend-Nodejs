55f25f947e71f907b1d2d63cc8f258f3f44c430a
nitesh kumar <niteshbhai0911@gmail.com>
2025-07-02 23:20:29 +0530

    what is oops and it js support oops

---
+++ Core_Web_&_Programming/OOPs/learn.js
@@ -0,0 +1,6 @@
+// OOPs (Object Oriented Programming)
+// kya javascript object oriented programming support karta hai answer is yes or no both
+// Javascript is prototype based language which primarly focus on functional style of programming.
+// first object oriented programming language is Small Talk
+
+// With ES6 we got support of classes, which is syntatic sugar over existring function.
