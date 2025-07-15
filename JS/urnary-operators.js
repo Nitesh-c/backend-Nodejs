bd9fc2aa97408f8230f9d122253a6a83912bb258
nitesh kumar <niteshbhai0911@gmail.com>
2025-07-15 22:22:31 +0530

    understand what is void and what we have to this- this is an operator it always return undefined

---
+++ JS/urnary-operators.js
@@ -0,0 +1,22 @@
+// unary operator in js - unary means -> involving only one
+/*
+unary plus
+unary minus
+increment
+decrement
+logical not
+bitwise not
+typeof() // we cannot say function or method
+void()
+delete // we can say it's a property of object
+*/
+
+// void() -- always return undefined
+// where do we use ?
+// for doing some operations but not want answer
+
+console.log(void(23))
+console.log(void("nitesh"))
+console.log(void(true))
+console.log(void([12,12,12,12,12]))
+console.log(void({a:23, b:134}))
