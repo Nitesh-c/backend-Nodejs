1d37f856f2974f81329ffe34c7b7dd9fc429f319
nitesh kumar <niteshbhai0911@gmail.com>
2025-07-31 00:54:11 +0530

    create some pattern in the night and then sleep

--- JS/DSA with JS/Pattern_programming/patternProgramming.js
+++ JS/DSA with JS/Pattern_programming/patternProgramming.js
@@ -3,9 +3,36 @@ const prompt = require("prompt-sync")();
 let userInp = +prompt("user_input: ");
 
 // very normal
+// for(let i = 1; i <= userInp; i++){
+//     for(let j = 1; j <= userInp; j++){
+//         process.stdout.write("*")
+//     }
+//     console.log();
+// }
+
+// right-angled triangle
+
+// for(let i = 1; i <= userInp; i++){
+//     for(let j = 1; j <= i; j++){
+// process.stdout.write("*")
+//     }
+//     console.log()
+// }
+
+// Inverted triagle
+// for(let i = 1; i <= userInp; i++){
+    // for(let j = userInp; j >= i; j--){
+        // process.stdout.write("*")
+    // }
+    // console.log();
+// }
+
+// Right-angled tringle
+let ascii = 65;
 for(let i = 1; i <= userInp; i++){
-    for(let j = 1; j <= userInp; j++){
-        process.stdout.write("*")
+    for(let j = 1; j <= i; j++){
+        process.stdout.write(`${String.fromCharCode(ascii)}`)
     }
     console.log();
+    ascii++
 }
