020770e74abc469aaae991b167014d36ebb237eb
nitesh kumar <niteshbhai0911@gmail.com>
2025-08-02 00:50:39 +0530

    i impliment smooth animationa using css and js

--- JS/Login_Signup/script.js
+++ JS/Login_Signup/script.js
@@ -2,17 +2,29 @@ let nameInputDiv = document.querySelector(".usernamefield");
 let loginBtn = document.querySelector("#login-btn");
 let underline = document.querySelector(".underline");
 let signupBtn = document.querySelector("#signup-btn");
-
+let heading = document.querySelector("#page-heading")
 loginBtn.addEventListener("click", function(){
-    nameInputDiv.style.display = "none"   
     signupBtn.classList.add("disable");
+    underline.style.transform = "translateX(100%)"
     loginBtn.classList.remove("disable");
-    underline.style.marginLeft = "auto"
+    nameInputDiv.style.height = "0px";
+    nameInputDiv.style.overflow = "hidden";
+    heading.style.opacity = 0;
+    setTimeout(()=>{
+        heading.style.opacity = 1;
+        heading.textContent = "Sign In"
+    }, 500)
 })
 
 signupBtn.addEventListener("click", function(){
-    nameInputDiv.style.display = "block"   
     loginBtn.classList.add("disable");
+    underline.style.transform = "translateX(0%)"
     signupBtn.classList.remove("disable");
-    underline.style.marginLeft = "0"
+    nameInputDiv.style.height = "3rem";
+    nameInputDiv.style.overflow = "hidden";
+    heading.style.opacity = 0;
+    setTimeout(()=>{
+        heading.style.opacity = 1;
+        heading.textContent = "Sign Up"
+    }, 500)
 })
